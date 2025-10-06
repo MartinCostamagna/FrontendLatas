f5831d931a6c248fef49347484031a145ad0a7f9
Martin Costamagna <martinandrecosta@gmail.com>
2025-10-06 14:06:22 -0300

    Front responsivo a pantallas pequeñas en login e inicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router';
 import { Inicio } from './pages/inicio/inicio';
 import { Login } from './pages/login/login';
-import { SeleccionarLista } from './pages/seleccionar-lista/seleccionar-lista';
 import { Listado } from './pages/listado/listado';
 import { Lata } from './pages/lata/lata';
 import { Estadistica } from './pages/estadistica/estadistica';
 import { NuevaLata } from './pages/nueva-lata/nueva-lata';
-import { EditarLata } from './pages/editar-lata/editar-lata';
 import { NuevaMarca } from './pages/nueva-marca/nueva-marca';
 import { NuevoTamano } from './pages/nuevo-tamano/nuevo-tamano';
 import { NuevoSabor } from './pages/nuevo-sabor/nuevo-sabor';
@@ -21,14 +19,16 @@ export const routes: Routes = [
         pathMatch: 'full',
         redirectTo: 'inicio'
     },
-    { path: 'inicio', component: Inicio },
     { path: 'login', component: Login },
-    { path: 'seleccionarLista', component: SeleccionarLista },
-    { path: 'listado', component: Listado },
+
+    { path: 'inicio', component: Inicio },
+
+    { path: 'latas', component: Listado },
     { path: 'lata', component: Lata },
+
     { path: 'estadistica', component: Estadistica },
+    
     { path: 'NuevaLata', component: NuevaLata },
-    { path: 'EditarLata', component: EditarLata },
     { path: 'NuevaMarca', component: NuevaMarca },
     { path: 'NuevoTamano', component: NuevoTamano },
     { path: 'NuevoSabor', component: NuevoSabor },
